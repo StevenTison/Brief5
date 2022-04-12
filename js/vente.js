5068b7cb7633bb931cc3676bf5c1c4f3a2b2e855
AlexLekieffre <alekieffre@gmail.com>
2022-04-12 12:00:13 +0200

    vente

--- js/vente.js
+++ js/vente.js
@@ -1,17 +1,61 @@
 const url = 'http://51.137.57.127:1337';
 const api = '/api/ventes/';
 
-fetch("http://51.137.57.127:1337/api/ventes?populate=*");
+fetch("http://51.137.57.127:1337/api/ventes?populate=*")
+    .then(function(res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function(value) {
 
-let eltVenteArticle = document.querySelector('div.ventes');
+        value.data.sort(function(a, b) {
+            return a.id - b.id;
+        });
 
-for (let i = 0; i < 5; i++) {
-    let eltVentes = document.createElement('a');
-    eltVenteArticle.appendChild(eltVentes);
-    eltVentes.classList.add('vente_article');
-    eltVentes.href = "#"
+        let n = 1
 
-    let eltPrix = document.createElement("p");
-    eltPrix.appendChild(eltVenteArticle)
-    eltPrix.innerText = url + vente.attributes.prix;
-}
+        for (let modif of value.data) {
+            modif.id = n;
+            n++;
+        }
+
+        let eltVenteArticle = document.querySelector('div.ventes');
+
+        for (let article of value.data) {
+            let eltVentes = document.createElement('a');
+            eltVenteArticle.appendChild(eltVentes);
+            eltVentes.classList.add('vente_article');
+            eltVentes.href = "#"
+            console.log(value.data);
+
+            let eltPrix = document.createElement("p");
+            eltVenteArticle.appendChild(eltPrix)
+            eltPrix.innerText = article.attributes.prix;
+            eltPrix.classList.add('prix_article');
+
+            let eltDuree = document.createElement("p");
+            eltVenteArticle.appendChild(eltDuree)
+            eltDuree.innerText = article.attributes.duree;
+            eltDuree.classList.add('duree_article');
+
+            let eltTitre = document.createElement("h3");
+            eltVenteArticle.appendChild(eltTitre)
+            eltTitre.innerText = article.attributes.titre
+            eltTitre.classList.add('titre_article');
+
+            let eltLogo = document.createElement("img")
+            eltVenteArticle.appendChild(eltLogo)
+            eltLogo.src = url + article.attributes.logo.data[0].attributes.url;
+            eltLogo.classList.add('logo_article');
+
+            eltImage = document.createElement("img")
+            eltVenteArticle.appendChild(eltImage)
+            eltImage.src = url + article.attributes.backvente.data[0].attributes.url;
+            eltImage.classList.add('img_article');
+
+        }
+    })
+    .catch(function(err) {
+
+    });
