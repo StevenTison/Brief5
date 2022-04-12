3e2bcee1a11f5f27126ab68f1a7157b1d4afbf90
AlexLekieffre <alekieffre@gmail.com>
2022-04-12 15:02:27 +0200

    création des div en java

--- js/vente.js
+++ js/vente.js
@@ -21,34 +21,47 @@ fetch("http://51.137.57.127:1337/api/ventes?populate=*")
         }
 
         let eltVenteArticle = document.querySelector('div.ventes');
-
+        // contenant
         for (let article of value.data) {
             let eltVentes = document.createElement('a');
             eltVenteArticle.appendChild(eltVentes);
             eltVentes.classList.add('vente_article');
             eltVentes.href = "#"
             console.log(value.data);
+            //top
+            let eltDivTop = document.createElement('div.top')
+            eltVenteArticle.appendChild(eltDivTop);
+            eltDivTop.classList.add('top')
+                //bot
+            let eltDivBot = document.createElement('div')
+            eltVenteArticle.appendChild(eltDivBot);
+            eltDivBot.classList.add('bot');
+
+
+
+
+            //Prix
 
             let eltPrix = document.createElement("p");
-            eltVenteArticle.appendChild(eltPrix)
+            eltDivTop.appendChild(eltPrix)
             eltPrix.innerText = article.attributes.prix;
             eltPrix.classList.add('prix_article');
-
+            //duree
             let eltDuree = document.createElement("p");
-            eltVenteArticle.appendChild(eltDuree)
+            eltDivTop.appendChild(eltDuree)
             eltDuree.innerText = article.attributes.duree;
             eltDuree.classList.add('duree_article');
-
+            //Titre
             let eltTitre = document.createElement("h3");
             eltVenteArticle.appendChild(eltTitre)
             eltTitre.innerText = article.attributes.titre
             eltTitre.classList.add('titre_article');
-
+            //logo
             let eltLogo = document.createElement("img")
-            eltVenteArticle.appendChild(eltLogo)
+            eltDivBot.appendChild(eltLogo)
             eltLogo.src = url + article.attributes.logo.data[0].attributes.url;
             eltLogo.classList.add('logo_article');
-
+            //background vente
             eltImage = document.createElement("img")
             eltVenteArticle.appendChild(eltImage)
             eltImage.src = url + article.attributes.backvente.data[0].attributes.url;
