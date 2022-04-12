1511251808b705b4437fecbbc3f5843be109ea72
AlexLekieffre <alekieffre@gmail.com>
2022-04-12 10:34:01 +0200

    1er commit alex

--- js/vente.js
+++ js/vente.js
@@ -1,12 +1,17 @@
-const url = 'http://51.137.57.127:1337'
-const api = '/api/ventes/'
+const url = 'http://51.137.57.127:1337';
+const api = '/api/ventes/';
 
-fetch("http://51.137.57.127:1337/api/ventes?populate=*")
+fetch("http://51.137.57.127:1337/api/ventes?populate=*");
 
-let eltVentes = document.querySelector('div.ventes');
+let eltVenteArticle = document.querySelector('div.ventes');
 
-for ( let ventes ) {
+for (let i = 0; i < 5; i++) {
     let eltVentes = document.createElement('a');
-    eltEtape.appendChild(eltVentes);
-    eltLink.classList.add('lien_article');
-    eltLink.href = "#";
+    eltVenteArticle.appendChild(eltVentes);
+    eltVentes.classList.add('vente_article');
+    eltVentes.href = "#"
+
+    let eltPrix = document.createElement("p");
+    eltPrix.appendChild(eltVenteArticle)
+    eltPrix.innerText = url + vente.attributes.prix;
+}
