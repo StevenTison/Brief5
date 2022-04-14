8423dddc6df48a592d29d9be9b642bb1244495c9
StevenTison <steven.tison22@gmail.com>
2022-04-14 12:24:16 +0200

    changement nom itinéraire

--- itinéraire/detail.js
+++ itinéraire/detail.js
@@ -131,7 +131,7 @@ fetch("http://51.137.57.127:1337/api/details?populate=*")
         let eltFlecheContenu = document.createElement('a');
         eltRetour.appendChild(eltFlecheContenu);
         eltFlecheContenu.classList.add('fleche_contenu');
-        eltFlecheContenu.href = "itineraire.html";
+        eltFlecheContenu.href = "itinéraire.html";
 
         let eltFleche = document.createElement('img');
         eltFlecheContenu.appendChild(eltFleche);
